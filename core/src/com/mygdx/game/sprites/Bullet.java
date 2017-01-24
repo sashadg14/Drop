5faf2f2ef3a7259a7b6af9bbe7a7376a70507304
alex <mamayko.1998@mail.ru>
2017-01-24 22:29:14 +0200

    tank

--- core/src/com/mygdx/game/sprites/Bullet.java
+++ core/src/com/mygdx/game/sprites/Bullet.java
@@ -2,28 +2,57 @@ package com.mygdx.game.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector3;
+import com.mygdx.game.states.PlayState;
+
+import static java.lang.StrictMath.PI;
+import static java.lang.StrictMath.sin;
+import static java.lang.StrictMath.tan;
 
 /**
  * Created by alex on 23.01.2017.
  */
 
-public class Bullet {
-private Texture bulletTexture;
+public class Bullet{
+
     private Vector3 vector3;
     private float angle;
     private int damage=1;
+    private long Time=0;
+    private float startX;
+    private float startY;
+
+    public Vector3 getPosition() {
+        return vector3;
+    }
+
     Bullet(float angle, float x, float y )
     {
         this.angle=angle;
         vector3 = new Vector3(x,y,0);
-        bulletTexture = new Texture("bullet.png");
+        startX=x;
+        startY=y;
     }
 
-    public void render(SpriteBatch sb)
+    public void render(SpriteBatch sb, Texture bulletTexture)
     {
+        if(System.currentTimeMillis()-Time>10)
+        {
+            Time = System.currentTimeMillis();
+            vector3.x+=12*sin(-angle*PI/180);
+            vector3.x-=startX;
+            vector3.y= (float) (vector3.x*-1/tan(angle*PI/180))+startY;
+            vector3.x+=startX;
+
+        }
+
         sb.begin();
-    //    sb.draw();
+        sb.draw(bulletTexture, vector3.x ,vector3.y,bulletTexture.getWidth()/2, 0,
+
+                bulletTexture.getWidth()* PlayState.getGOWHT()/2f, bulletTexture.getHeight()*PlayState.getGOWHT()/2f,
+
+                1,1,angle, 0,0,bulletTexture.getWidth(), bulletTexture.getHeight(), false, false);
         sb.end();
 
     }
--- core/src/com/mygdx/game/sprites/Tank.java
+++ core/src/com/mygdx/game/sprites/Tank.java
@@ -4,11 +4,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.states.PlayState;
 
 import static java.lang.Math.PI;
 import static java.lang.Math.atan;
+import static java.lang.StrictMath.tan;
 
 /**
  * Created by alex on 10.01.2017.
@@ -17,18 +20,26 @@ import static java.lang.Math.atan;
 public class Tank {
     private Vector3 positionTank;
     private Vector3 positionDulo;
+
     private Texture tank;
     private Texture dulo;
+    private Texture bulletTexture;
+
     private Wheels wheels;
     float angl;
+    private Array<Bullet> bullet;
+    private long Time=0;
 
     public Tank(int x, int y)
 
     {   tank = new Texture("tank.png");
         dulo = new Texture("dulo.png");
+        bulletTexture = new Texture("bullet.png");
+
         positionTank = new Vector3(x,y,0);
         positionDulo = new Vector3(positionTank.x+tank.getWidth()*PlayState.getGOWHT()/2- dulo.getWidth()* PlayState.getGOWHT(),y,0);
         wheels= new Wheels(positionTank.x+tank.getWidth()*PlayState.getGOWHT()/2,positionTank.y);
+        bullet = new Array<Bullet>();
     }
 
     public Vector3 getPositionTank()
@@ -55,11 +66,12 @@ public class Tank {
     public Texture getDuloTexture() {
         return dulo;
     }
-
     public void handleInput()
     {
         if(Gdx.input.isTouched())
         {
+
+
             if(Gdx.input.getX()-(getPositionTank().x+getTankTexture().getWidth()*PlayState.getGOWHT()/2)>4)
         {
             angl= (float) (atan(1/(Gdx.graphics.getHeight()-Gdx.input.getY()-getTankTexture().getHeight()*PlayState.getGOWHT()+33)* (getPositionTank().x+getTankTexture().getWidth()*PlayState.getGOWHT()/2-Gdx.input.getX()) )*180/PI);
@@ -68,7 +80,13 @@ public class Tank {
                 angl=-90;
             wheels.setPositionWeel(getPositionTank().x+tank.getWidth()*PlayState.getGOWHT()/2, getPositionTank().y);
 
-            System.out.println("Y= "+(Gdx.graphics.getHeight()-Gdx.input.getY() -getTankTexture().getHeight()*PlayState.getGOWHT()+33)+" X= "+(getPositionTank().x+getTankTexture().getWidth()*PlayState.getGOWHT()/2-Gdx.input.getX())+"\n"+"alpf="+angl+"\n");
+            if(System.currentTimeMillis()-Time>300)
+            {
+                Time = System.currentTimeMillis();
+                bullet.add(new Bullet(angl,getPositionDulo().x,getPositionDulo().y));
+            }
+            System.out.println("Y= "+getPositionDulo().y+"  X="+getPositionDulo().x);
+           // System.out.println("Y= "+(Gdx.graphics.getHeight()-Gdx.input.getY() -getTankTexture().getHeight()*PlayState.getGOWHT()+33)+" X= "+(getPositionTank().x+getTankTexture().getWidth()*PlayState.getGOWHT()/2-Gdx.input.getX())+"\n"+"alpf="+angl+"\n");
         }
         else
             if(Gdx.input.getX()-(getPositionTank().x+getTankTexture().getWidth()*PlayState.getGOWHT()/2)<-4)
@@ -81,16 +99,37 @@ public class Tank {
 
             wheels.setPositionWeel(getPositionTank().x+tank.getWidth()*PlayState.getGOWHT()/2, getPositionTank().y);
 
-            System.out.println("Y= "+(Gdx.graphics.getHeight()-Gdx.input.getY() -getTankTexture().getHeight()*PlayState.getGOWHT()+33)+" X= "+(getPositionTank().x+getTankTexture().getWidth()*PlayState.getGOWHT()/2-Gdx.input.getX())+"\n"+"alpf="+angl+"\n");
+            if(System.currentTimeMillis()-Time>300)
+            {
+                Time = System.currentTimeMillis();
+                bullet.add(new Bullet(angl,getPositionDulo().x,getPositionDulo().y));
+            }
+            System.out.println("Y= "+getPositionDulo().y+"  X="+getPositionDulo().x);
+            //System.out.println("Y= "+(Gdx.graphics.getHeight()-Gdx.input.getY() -getTankTexture().getHeight()*PlayState.getGOWHT()+33)+" X= "+(getPositionTank().x+getTankTexture().getWidth()*PlayState.getGOWHT()/2-Gdx.input.getX())+"\n"+"alpf="+angl+"\n");
         }
+            if(System.currentTimeMillis()-Time>300)
+            {
+                Time = System.currentTimeMillis();
+                bullet.add(new Bullet(angl,getPositionDulo().x,getPositionDulo().y));
+            }
         }
 
     }
 
     public void render(SpriteBatch sb)
     {
-        sb.begin();
+        for(Bullet bull: bullet )
+        {   if( (bull.getPosition().y<Gdx.graphics.getHeight()&&bull.getPosition().x<Gdx.graphics.getWidth())
+                &&(bull.getPosition().y>0&&bull.getPosition().x>0) )
+            bull.render(sb, bulletTexture);
+            else
+        {
 
+            bullet.removeValue(bull,true);
+            System.out.println("DELETED");
+        }
+        }
+            sb.begin();
         sb.draw(getDuloTexture(), (getPositionDulo().x ),(getPositionDulo().y),getDuloTexture().getWidth()/2, 0,
 
                 getDuloTexture().getWidth()*PlayState.getGOWHT()/2f, getDuloTexture().getHeight()*PlayState.getGOWHT()/2f,
